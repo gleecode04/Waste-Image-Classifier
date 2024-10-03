efac985f641974e1fef9a15e08edf083101e733d
gleecode04 <147885240+gleecode04@users.noreply.github.com>
2024-10-03 13:29:12 -0400

    Update main.py

--- API/main.py
+++ API/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
 
 MODEL = tf.keras.models.load_model("../saved_models/1")
 
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+CLASS_NAMES = ["plastic", "glass", "paper", "steel"]
 
 @app.get("/ping")
 async def ping():
